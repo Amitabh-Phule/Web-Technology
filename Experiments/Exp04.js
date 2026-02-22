c56eb1ff3fee78cc4b262c5905cb98fd73e3cf32
AdityaSuryakar <suryakaraditya@gmail.com>
2026-02-22 22:51:06 +0530

    WT Project

---
+++ Experiments/Exp04.js
@@ -0,0 +1,7 @@
+//==============DOM=================
+//Document Object Manipulation
+//HTML can create form but it cannot think,js make form smart
+//form validation:checking user inputs before sending data to server
+//DOM Manipultion
+//it means changing html using js
+//eg.showing tesxt color,display success message,clear  input fieldss 
