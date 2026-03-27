2f03dcebaf4616dad080b28be42a953c84d16020
AdityaSuryakar <suryakaraditya@gmail.com>
2026-03-27 08:12:23 +0530

    Updated Experiments

--- Experiments/Exp04.js
+++
@@ -1,7 +0,0 @@
-//==============DOM=================
-//Document Object Manipulation
-//HTML can create form but it cannot think,js make form smart
-//form validation:checking user inputs before sending data to server
-//DOM Manipultion
-//it means changing html using js
-//eg.showing tesxt color,display success message,clear  input fieldss 
