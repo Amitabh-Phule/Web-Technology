3e3b7b4f3ee230ee9a22d5bd68b288f5b656731c
Amitabh-Phule <amitabhphule4@gmail.com>
2026-04-30 02:05:53 +0530

    Wipe remote repository: removing all tracked folders to restart structure

--- Lab/Exp09/my-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { StudentList } from './student-list/student-list';
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    {path: 'students', component: StudentList}
-];
-
